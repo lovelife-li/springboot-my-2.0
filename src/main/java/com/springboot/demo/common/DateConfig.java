7b4aadd89572b6b74bd1018feb97403fe00d2249
lidebo <ldbdream@qq.com>
2019-07-05 17:07:55 +0800

    save

--- src/main/java/com/springboot/demo/common/DateConfig.java
+++ src/main/java/com/springboot/demo/common/DateConfig.java
@@ -18,6 +18,8 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -177,6 +179,19 @@ public class DateConfig {
         objectMapper.registerModule(javaTimeModule);
         return objectMapper;
     }
+
+//    @Bean
+//    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer(
+//            JacksonProperties jacksonProperties) {
+//        final String dateFormat = StringUtils.isBlank(jacksonProperties.getDateFormat()) ? "yyyy-MM-dd HH:mm:ss"
+//                : jacksonProperties.getDateFormat();
+//        return builder -> {
+//            builder.serializerByType(LocalDateTime.class,
+//                    new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+//            builder.deserializerByType(LocalDateTime.class,
+//                    new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(dateFormat)));
+//        };
+//    }
 
 
 }
